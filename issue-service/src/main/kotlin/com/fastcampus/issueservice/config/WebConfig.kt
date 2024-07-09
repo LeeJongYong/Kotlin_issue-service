34f7c4b12eca49107a29626d324450b81f3796ef
teddy <jongyong924@gmail.com>
2024-07-09 20:07:30 +0900

    Kotlin - Membership Auth API - api connection - frontend connection

--- issue-service/src/main/kotlin/com/fastcampus/issueservice/config/WebConfig.kt
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/config/WebConfig.kt
@@ -1,5 +1,9 @@
 package com.fastcampus.issueservice.config
 
+import com.fastcampus.issueservice.exception.UnauthorizedException
+import com.fasterxml.jackson.annotation.JsonProperty
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -7,6 +11,9 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
@@ -21,10 +28,22 @@ class WebConfig(
         }
     }
 
+    // web 정적 리소스에 자동 접근하기 위한 경로 설정
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/**")      // 모든 경로 : /**
+            .addResourceLocations(
+                "classpath:/META-INF/resources/",
+                "classpath:/resources/",
+                "classpath:/static/",
+                "classpath:/public/",
+            )
+    }
 }
 
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver(
+    @Value("\${auth.url}") val authUrl: String,
+) : HandlerMethodArgumentResolver {
 
     override fun supportsParameter(parameter: MethodParameter): Boolean =
         AuthUser::class.java.isAssignableFrom(parameter.parameterType)
@@ -36,18 +55,31 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         binderFactory: WebDataBinderFactory?
     ): Any? {
 
-        return AuthUser(
-            userId = 1,
-            username = "테스트",
-        )
+        // 헤더의 Authorization가 없을경우 인증 실패로 간주하여 exception발생
+        val authHeader = webRequest.getHeader("Authorization") ?: throw UnauthorizedException()
+
+        return runBlocking {
+            WebClient.create()
+                .get()
+                .uri(authUrl)
+                .header("Authorization", authHeader)    // Bearer adslfknlk1nlkdsamlkdmlkdmlq..
+                .retrieve()
+                .awaitBody<AuthUser>()
+        }
+//        return AuthUser(
+//            userId = 1,
+//            username = "테스트",
+//        )
     }
 
 
 }
 
 data class AuthUser(
+    @JsonProperty("id")         // JsonProperty annotation을 사용하면 자동으로 매핑된다.
     val userId: Long,
     val username: String,
+    val email: String,
     val profileUrl: String? = null,
 )
 
--- user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
@@ -2,36 +2,72 @@ package com.fastcampus.userservice.controller
 
 import com.fastcampus.userservice.model.*
 import com.fastcampus.userservice.service.UserService
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.*
+import java.io.File
 
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController(
     private val userService: UserService,
 ) {
-
+    // 회원가입
     @PostMapping("/signup")
     suspend fun signUp(@RequestBody request: SignUpRequest) {       // 입력받은 json 데이터를 활용
         userService.signUp(request)
     }
 
+    // 로그인
     @PostMapping("/signin")
     suspend fun signIn(@RequestBody signInRequest: SignInRequest) : SignInResponse {        // 입력받은 json 데이터를 활용
         return userService.signIn(signInRequest)
     }
 
+    // 로그아웃
     @DeleteMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     suspend fun logout(@AuthToken token : String) {     // 생성 annotation을 활용하여 핸들러에서 캐치
         userService.logout(token)
     }
 
+    // 내 정보 조회
     @GetMapping("/me")
     suspend fun get(
         @AuthToken token: String
     ): MeResponse {
         return MeResponse(userService.getByToken(token))
+    }
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(@PathVariable userId: Long) : Map<String, String> {
+        return mapOf("repoter" to userService.get(userId).username)
+    }
+
+    @PostMapping("/{id}", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    suspend fun edit(
+        @PathVariable id: Long,
+        @ModelAttribute request: UserEditRequest,
+        @AuthToken token: String,
+        @RequestPart("profileUrl") filePart: FilePart,
+    ){
+
+        val orgFilename = filePart.filename()
+        var filename: String? = null
+        if(orgFilename.isNotEmpty()){
+            // .jpg
+            val ext = orgFilename.substring(orgFilename.lastIndexOf(".") + 1)
+            filename = "${id}.${ext}"
+
+            val file = File(ClassPathResource("/images/").file, filename)
+
+            filePart.transferTo(file).awaitSingleOrNull()
+        }
+
+        userService.edit(token, request.username, filename)
     }
 
 }
